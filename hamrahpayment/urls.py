e4db9a7b3cfade52b9dc0587e49800bf29eced36
hosein asadzadeh <hoseinasadzadeh@hoseins-MacBook-Pro.local>
2025-09-08 17:42:27 +0330

    create users app

--- hamrahpayment/urls.py
+++ hamrahpayment/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
+    path('', include('users.urls')),
 ]
 
 if settings.IS_DEVELOP:
